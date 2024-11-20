436e327173c24a95aac452c19ff7995f137ca375
Moaz Ragab <moazra06@gmail.com>
2024-11-20 21:38:56 +0200

    complete footer scetion, add many variables to config file and change fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 // tailwind.config.js
-module.exports = {
+tailwind.config = {
     theme: {
+       
       extend: {
         animation: {
           // Custom spin animation with adjusted speed (2s for one rotation)
-          'spin-slow': 'spin 3s linear infinite',
+          spin_slow: 'spin 6s linear infinite',
           'spin-fast': 'spin 0.5s linear infinite',
+        },
+        gridTemplateColumns:{
+            'auto': 'repeat(auto-fit,minmax(200px,1fr))'
+        },
+        fontFamily:{
+         Outfit:["Outfit", "sans-serif"], 
+         Ovo:["Ovo", "serif"],
+        },
+        colors:{
+           lightHover : '#f5f2f6',
+           darkHover : '#2a004a'
+        },
+        boxShadow:{
+          'black': '4px 4px 0 #000',
+          'white': '4px 4px 0 #fff',
         },
       },
     },
